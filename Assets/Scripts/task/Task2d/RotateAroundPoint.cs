1a818aef388757540147f79b02114bd090cc17ce
mikenigro <mikenigro99@gmail.com>
2022-04-19 00:13:33 +0200

    fix vari

--- Assets/Scripts/task/Task2d/RotateAroundPoint.cs
+++ Assets/Scripts/task/Task2d/RotateAroundPoint.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
 
-public class RotateAroundPoint : Action
-{
+public class RotateAroundPoint : Action {
+	public SharedFloat vel;
 	public override void OnStart()
 	{
 		
 	}
 
-	public override TaskStatus OnUpdate()
-	{
+	public override TaskStatus OnUpdate() {
+		transform.position += transform.right * vel.Value*Time.deltaTime;
 		return TaskStatus.Success;
 	}
 }
