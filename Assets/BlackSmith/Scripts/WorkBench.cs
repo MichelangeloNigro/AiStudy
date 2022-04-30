d88bb16e13e96df4e48f6e59139faa79f52b3d61
mikenigro <mikenigro99@gmail.com>
2022-04-30 08:15:52 +0200

    workbenchfucntion tag to be created

--- Assets/BlackSmith/Scripts/WorkBench.cs
+++ Assets/BlackSmith/Scripts/WorkBench.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,4 +7,17 @@ public class WorkBench : MonoBehaviour {
   public int metal;
   public int wood;
   public int cloth;
+  public int missingMetal;
+  public int missingWood;
+  public int missingCloth;
+  private Worker worker;
+  private void Start() {
+    worker = FindObjectOfType<Worker>();
+  }
+
+  public void setMissingMaterials() {
+    missingCloth = worker.current.cloth - cloth;
+    missingWood = worker.current.wood - wood;
+    missingMetal = worker.current.metal - metal;
+  }
 }
--- Assets/BlackSmith/Scripts/Worker.cs
+++ Assets/BlackSmith/Scripts/Worker.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,41 @@ using UnityEngine;
 public class Worker : MonoBehaviour {
     public RecipeSO current;
     public bool isBusy;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public materiale carrying;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("WorkBench")) {
+            var bench=other.GetComponent<WorkBench>();
+            switch (carrying) {
+                case materiale.wood:
+                    bench.wood++;    
+                    break;
+                case materiale.metal:
+                    bench.metal++;
+                    break;
+                case materiale.cloth:
+                    bench.cloth++;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            bench.setMissingMaterials();
+        }
+        if (other.CompareTag("Metal")) {
+            carrying = materiale.metal;
+        }
+        if (other.CompareTag("Wood")) {
+            carrying = materiale.wood;
+        }
+        if (other.CompareTag("Cloth")) {
+            carrying = materiale.cloth;
+        }
     }
+}
+
+
+public enum materiale {
+    wood,
+    metal,
+    cloth
 }
